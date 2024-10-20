debb101ed2e6af9b35f5fea9415c0934a4434b81
Shashi Kumar Jha <shahsi.jha1617@gmail.com>
2024-10-20 22:20:35 +0530

    Initial Update

---
+++ index.js
@@ -0,0 +1,3 @@
+const file_name = "This is index.js";
+
+console.log(`This is ${file_name} file it means this is root file`);
---
+++ script.js
@@ -0,0 +1,5 @@
+const Name = "Shashi Kumar Jha";
+
+console.log(`My Name is ${Name}`);
+
+const father = "Prem Kumar Jha";
