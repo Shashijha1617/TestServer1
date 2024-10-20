1cb15fec667e64f39653bbdea13391f0eac7c213
Shashi Kumar Jha <shahsi.jha1617@gmail.com>
2024-10-20 22:25:35 +0530

    Superman

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const file_name = "This is index.js";
 
 console.log(`This is ${file_name} file it means this is root file`);
+
+console.log("I Am Superman");
+console.log("I Am batman");
+console.log("I Am spiderman");
